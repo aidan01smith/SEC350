67ffed961c4acabe48adde95b34ca4d95dae3f5a
aidan01smith <143528066+aidan01smith@users.noreply.github.com>
2026-04-29 17:51:30 -0400

    Uncomment wks01-aidan DNS record addition

--- test/winders/conf_dns.ps1
+++ test/winders/conf_dns.ps1
@@ -4,7 +4,7 @@ Add-DnsServerResourceRecordA -Name "fw-mgmt-aidan" -ZoneName "aidan.local" -IPv4
 Add-DnsServerResourceRecordA -Name "web01-aidan" -ZoneName "aidan.local" -IPv4Address "172.16.50.3"
 Add-DnsServerResourceRecordA -Name "jump-aidan" -ZoneName "aidan.local" -IPv4Address "172.16.50.4"
 Add-DnsServerResourceRecordA -Name "mgmt01-aidan" -ZoneName "aidan.local" -IPv4Address "172.16.150.10"
-# Add-DnsServerResourceRecordA -Name "wks01-aidan" -ZoneName "aidan.local" -IPv4Address "172.16.150.50"
+Add-DnsServerResourceRecordA -Name "wks01-aidan" -ZoneName "aidan.local" -IPv4Address "172.16.150.50"
 Add-DnsServerResourceRecordA -Name "wazuh-aidan" -ZoneName "aidan.local" -IPv4Address "172.16.200.10"
 Add-DnsServerResourceRecordA -Name "mgmt02-aidan" -ZoneName "aidan.local" -IPv4Address "172.16.150.10"
 
