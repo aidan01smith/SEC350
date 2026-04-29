cd5383935a3e01fe70d43d119883bf554ecf9547
aidan01smith <143528066+aidan01smith@users.noreply.github.com>
2026-04-29 17:49:02 -0400

    Add PowerShell script to install CA features

---
+++ test/winders/install_CA.ps1
@@ -0,0 +1,11 @@
+Install-WindowsFeature -Name AD-Certificate, ADCS-Cert-Authority -IncludeManagementTools
+
+# Configure as Enterprise Root CA
+Install-AdcsCertificationAuthority `
+    -CAType EnterpriseRootCA `
+    -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" `
+    -KeyLength 2048 `
+    -HashAlgorithmName SHA256 `
+    -ValidityPeriod Years `
+    -ValidityPeriodUnits 10 `
+    -Force
